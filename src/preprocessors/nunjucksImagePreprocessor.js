1fb06890450a5b47aec5d5d0b717ba0524d156cc
GordonAnsell <moving@ga1964.com>
2021-10-09 06:14:28 +0100

    Update nunjucksImagePreprocessor.js

--- src/preprocessors/nunjucksImagePreprocessor.js
+++ src/preprocessors/nunjucksImagePreprocessor.js
@@ -65,6 +65,7 @@ class NunjucksImagePreprocessor
                 let rep;
                 if (rss) {
                     rep = `{% simpleimg "${url}"`;
+                    syslog.warning('here');
                 } else {
                     rep = `{% img "${url}"`;
                 }
