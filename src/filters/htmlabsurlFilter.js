d04f87e74fce73cec6e539c1efd9950c5a25ae7d
GordonAnsell <moving@ga1964.com>
2021-10-19 06:43:27 +0100

    Update htmlabsurlFilter.js

--- src/filters/htmlabsurlFilter.js
+++ src/filters/htmlabsurlFilter.js
@@ -51,8 +51,11 @@ function htmlabsurlFilter(content, base)
     links = Array.from([... new Set(links)]);
 
     for (let l of links) {
-        //syslog.error("Replacing " + l + " with " + absurl(l, base));
         ret = string.replaceAll(ret, l, absurl(l, base));
+    }
+
+    if (ret.includes(base.trim() + base.trim())) {
+        syslog.error(`URL returned from htmlabsurlFilter contains double base.`)
     }
 
     //ret = string.replaceAll(ret, base + base, base);
