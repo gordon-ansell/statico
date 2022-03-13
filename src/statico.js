89dd2fbb43f0dcb516fb073efa558f4eb3f55050
GordonAnsell <moving@ga1964.com>
2022-03-13 06:12:57 +0000

    Update statico.js

--- src/statico.js
+++ src/statico.js
@@ -352,7 +352,7 @@ class Statico
             let l1 = others.length;
             others.filter(file => {
                 let s = fs.statSync(file);
-                let d1 = this.#startTime.getMilliseconds() - 3600000;
+                let d1 = this.#startTime - 3600000;
                 return s.mtimeMs > d1;
             });
             let l2 = others.length;
