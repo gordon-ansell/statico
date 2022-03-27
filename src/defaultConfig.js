efbf61b658df889c4343aea9d9fa6581a89f08f5
GordonAnsell <moving@ga1964.com>
2022-03-27 07:41:53 +0100

    Update defaultConfig.js

--- src/defaultConfig.js
+++ src/defaultConfig.js
@@ -36,15 +36,6 @@ module.exports = function(configData) {
             ignoreFilesFirst: undefined,
             ignoreExts: ['.sh'],
         },
-        fsParserDataDirFilters: {
-            allowPaths: undefined,
-            ignorePaths: ['_'],
-            ignoreDirs: undefined,
-            allowFiles: undefined,
-            ignoreFiles: undefined,
-            ignoreFilesFirst: undefined,
-            ignoreExts: undefined,
-        },
         templateHandlerDefaults: {
             nunjucks: {
                 exts: ['njk'],
