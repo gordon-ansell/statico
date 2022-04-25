d73feb30076d8e401eb943f33b157a64807100ca
GordonAnsell <moving@ga1964.com>
2022-04-25 06:50:57 +0100

    Update videolinkShortcode.js
    BBC videos.

--- src/shortcodes/videolinkShortcode.js
+++ src/shortcodes/videolinkShortcode.js
@@ -156,6 +156,90 @@ class VideoLinkShortcode extends NunjucksShortcode
             }
             this.config.schema[context.ctx.permalink].addVideo(url, saveData);
 
+        } else if ('bbc' === type) {
+
+            let id;
+            let imgid;
+            if (kwargs.id) {
+                if (-1 !== kwargs.id.indexOf('/')) {
+                    let sp = kwargs.id.split('/');
+                    imgid = sp[0];
+                    id = sp[1];
+                }
+                kwargs.src = "https://www.bbc.co.uk/{sect}/av-embeds/" + id;
+                delete kwargs.id;
+            } else {
+                syslog.error(`Video links for BBC require the 'id' parameter: ${context.ctx.permalink}.`);
+            }
+
+            if (!kwargs.description && kwargs.caption) {
+                kwargs.description = kwargs.caption;
+            }
+
+            if (kwargs.section) {
+                kwargs.src = kwargs.src.replace('{sect}', kwargs.section);
+            }
+
+            let meta = {};
+            for (let item of ['description', 'name', 'uploadDate']) {
+                if (kwargs[item]) {
+                    if ('uploadDate' == item) {
+                        let d = new MultiDate(kwargs[item]);
+                        meta[item] = d.iso;
+                    } else {
+                        meta[item] = kwargs[item];
+                    }
+                    delete kwargs[item];
+                } else {
+                    if (this.config.schemaWarnings) {
+                        syslog.warning(`BBC video links should have the '${item}' parameter: ${context.ctx.permalink}.`);
+                    }
+                }
+            }
+
+            ret += `<figure class="videolink aspect-ratio aspect-ratio--4x5">`
+            ret += `<iframe width="400" height="500" frameborder="0"`
+            for (let idx in kwargs) {
+                if (!idx.startsWith('__')) {
+                    if ('src' == idx) {
+                        ret += ` ${srcName}="${kwargs[idx]}"`;
+                    } else {
+                        ret += ` ${idx}="${kwargs[idx]}"`;
+                    }
+                }
+            }
+            ret += ' allowfullscreen>';
+            ret += '</iframe>';
+            if (kwargs.caption) {
+                ret += '<figcaption>' + kwargs.caption + '</figcaption>';
+            }
+            ret += '</figure>';
+
+            let url = kwargs.src;
+            delete kwargs.src;
+            delete kwargs.class;
+
+            let saveData = {
+                embedUrl: url,
+                contentUrl: kwargs.url,
+                thumbnailUrl: `https://ichef.bbci.co.uk/images/ic/688xn/${imgid}.jpg`
+            }
+
+            if (kwargs.caption) {
+                saveData.caption = kwargs.caption;
+            }
+
+            for (let idx in meta) {
+                if (!idx.startsWith('__')) {
+                    saveData[idx] = meta[idx];
+                }
+            }
+
+            if (!this.config.schema[context.ctx.permalink]) {
+                this.config.schema[context.ctx.permalink] = new Schema(this.config);
+            }
+            this.config.schema[context.ctx.permalink].addVideo(url, saveData);
+
         } else {
             syslog.error(`'${type}' is an unsupported video link type: ${context.ctx.permalink}.`);
         } 
